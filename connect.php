0aadafb2203854f15915b732532f85987e37e0ab
Zongkung <nontasit2411@gmail.com>
2025-01-16 18:09:32 +0700

    ๅ

--- connect.php
+++ connect.php
@@ -2,8 +2,8 @@
 // ข้อมูลการเชื่อมต่อฐานข้อมูล
 $host = "wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"; // MySQL Hostname
 $username = "vsy2jkwcvsmlks80"; // MySQL Username
-$password = "yksdci3wwkvudvu2"; // MySQL Password
-$dbname = "lyalxrwicbmvdtkj"; // ชื่อฐานข้อมูล (เปลี่ยน XXX เป็นชื่อฐานข้อมูลจริง)
+$password = "ha2ewmeethfq7wz1"; // MySQL Password
+$dbname = "lyalxrwicbmvdtkj"; // ชื่อฐานข้อมูล
 $port = 3306; // MySQL Port (ค่าเริ่มต้นคือ 3306)
 
 // สร้างการเชื่อมต่อ
--- sent_data/sent_data.php
+++ sent_data/sent_data.php
@@ -2,7 +2,7 @@
 // ข้อมูลการเชื่อมต่อฐานข้อมูล
 $host = "wyqk6x041tfxg39e.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"; // MySQL Hostname
 $username = "vsy2jkwcvsmlks80"; // MySQL Username
-$password = "yksdci3wwkvudvu2"; // MySQL Password
+$password = "ha2ewmeethfq7wz1"; // MySQL Password
 $dbname = "lyalxrwicbmvdtkj"; // ชื่อฐานข้อมูล
 $port = 3306; // MySQL Port (ค่าเริ่มต้นคือ 3306)
 
